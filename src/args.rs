a3da185d82b4789edb1e4b03a9ea705cd2724d20
Yoel Jacobsen <yoel.jacobsen@gmail.com>
2021-04-29 11:43:26 +0300

    Verify cred

--- src/args.rs
+++ src/args.rs
@@ -4,39 +4,65 @@ use clap::{Clap, ArgGroup};
 #[clap(author, about, version)]
 #[clap(group = ArgGroup::new("CONTAINER").required(false))]
 pub struct Opts {
+    // Test flags
+    #[clap(long = "verify_cred", about = "Just verify the credentials work")]
+    pub verify_cred: bool,
+
+    // Computational Flags
     #[clap(short = 'c', long = "cores", about = "Number of CPU cores", default_value = "4")]
     pub cores: i32,
+
     #[clap(short = 'm', long = "mem", about = "RAM size in GB", default_value = "8")]
     pub mem: i32,
+
     #[clap(short = 'g', long = "gpus", about = "Number of GPU devices", requires_all = &["gpu_type", "framework"])]
     pub gpus: Option<i32>,
+
     #[clap(name = "gpu_type", long = "gpu_type", about = "GPU Type", arg_enum)]
     pub gpu_type: Option<GPUType>,
-    #[clap(short = 'n', long = "name", default_value = "Unnamed job")]
-    pub name: String,
-    #[clap(short = 'v', long = "verbose", parse(from_occurrences))]
-    pub verbose: i32,
+
+    // Computational Runtime
     #[clap(short = 'f', long = "framework", arg_enum, group = "CONTAINER")]
     pub framework: Option<Framework>,
+
     #[clap(long = "container", group = "CONTAINER")]
     pub container: Option<String>,
+
+    // Environment
     #[clap(short = 'u', long = "user", default_value = "PI")]
     pub user: String,
+
     #[clap(long = "working_directory", env = "PWD", required = false)]
     pub wd: String,
+
     #[clap(short = 'w', long = "wait", about = "Block until job is finished?")]
     pub wait: bool,
+
+    // AWS Resources
     #[clap(short = 's', long = "spot", about = "Use SPOT (true) or On-Demand (false)")]
     pub spot: bool,
+
+    // Job meta-data
+    #[clap(short = 'n', long = "name", default_value = "Unnamed job")]
+    pub name: String,
+
     #[clap(long = "monitor", about = "Should the job be monitored", requires_all = &["monitor_interval", "emails"])]
     pub monitor: bool,
+
     #[clap(name = "monitor_interval",
            long = "monitor_interval",
            about = "Intervals for status mails",
            default_value_if("monitor", None, "1"))]
     monitor_interval: Option<i32>,
+
     #[clap(long = "emails", multiple = true, about = "e-mail addresses for status notifications")]
     pub emails: Option<Vec<String>>,
+
+    // General
+    #[clap(short = 'v', long = "verbose", parse(from_occurrences))]
+    pub verbose: i32,
+
+    // The command
     pub command: String,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use engage::args;
 use rusoto_batch::{Batch, BatchClient, DescribeJobDefinitionsRequest};
-//use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
 use rusoto_core::Region;
+use std::time::Duration;
+use rusoto_core::credential::ChainProvider;
+
+use rusoto_core::request::HttpClient;
+use rusoto_s3::{S3, S3Client};
+
 
 #[tokio::main]
 async fn main() {
     let opts: args::Opts = args::parse();
     println!("{:?}", opts);
+    if opts.verify_cred {
+        let mut provider = ChainProvider::new();
+        provider.set_timeout(Duration::from_secs(200));
+        let s3client = S3Client::new_with(
+                HttpClient::new().expect("failed to create request dispatcher"),
+            provider,
+        Region::UsEast1,
+    );
+        println!("Attempting to verify credentials using S3 bucket listing");
+        match s3client.list_buckets().await {
+        Err(e) => println!("Error listing buckets: {}", e),
+        Ok(buckets) => println!("Buckets found: {:?}", buckets),
+    };       }
 
     let client = BatchClient::new(Region::UsWest1);
     let request = DescribeJobDefinitionsRequest::default();
     let response = client.describe_job_definitions(request).await.unwrap();
 
-    println!("Resonse: {:?}", response);
+    println!("Response: {:?}", response);
     println!("Depth: {}", args::count_params(&opts.command));
 }
