da54e6b00e0ea0436f8926fc1fbd783d6484a45c
Yoel Jacobsen <yoel.jacobsen@gmail.com>
2021-05-05 10:24:51 +0300

    Verify cred

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use engage::args;
 use rusoto_batch::{Batch, BatchClient, DescribeJobDefinitionsRequest};
+use rusoto_core::credential::ChainProvider;
 use rusoto_core::Region;
 use std::time::Duration;
-use rusoto_core::credential::ChainProvider;
 
 use rusoto_core::request::HttpClient;
-use rusoto_s3::{S3, S3Client};
-
+use rusoto_s3::{S3Client, S3};
 
 #[tokio::main]
 async fn main() {
@@ -16,15 +15,16 @@ async fn main() {
         let mut provider = ChainProvider::new();
         provider.set_timeout(Duration::from_secs(200));
         let s3client = S3Client::new_with(
-                HttpClient::new().expect("failed to create request dispatcher"),
+            HttpClient::new().expect("failed to create request dispatcher"),
             provider,
-        Region::UsEast1,
-    );
+            Region::UsEast1,
+        );
         println!("Attempting to verify credentials using S3 bucket listing");
         match s3client.list_buckets().await {
-        Err(e) => println!("Error listing buckets: {}", e),
-        Ok(buckets) => println!("Buckets found: {:?}", buckets),
-    };       }
+            Err(e) => println!("Error listing buckets: {}", e),
+            Ok(buckets) => println!("Buckets found: {:?}", buckets),
+        };
+    }
 
     let client = BatchClient::new(Region::UsWest1);
     let request = DescribeJobDefinitionsRequest::default();
